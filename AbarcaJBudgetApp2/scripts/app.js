f913dcc67155d25b12ab26900f68738d5163fa09
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-05 00:22:16 -0700

    Refactor CheckVendorNameLength function

--- AbarcaJBudgetApp2/scripts/app.js
+++ AbarcaJBudgetApp2/scripts/app.js
@@ -262,7 +262,7 @@ function CheckVendorNameLength(vendorName) {
     //check the length of a vendor name 
     let newVendorName;
     if (vendorName.length > 18) {
-        newVendorName = vendorName.slice(0, 18) + "...";
+        newVendorName = vendorName.slice(0, 13) + "..."; // was (0, 18) previously
     } else {
         newVendorName = vendorName;
     }
